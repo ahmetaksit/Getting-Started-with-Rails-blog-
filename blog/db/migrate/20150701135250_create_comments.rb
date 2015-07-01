7534efbf78ebe52b968bd3273f5060c9628a4a67
Ahmet AKŞİT <ahmetaksit@gmail.com>
2015-07-01 16:58:13 +0300

    6.1 Generating a Model - 2 db:migrate

--- blog/db/migrate/20150701135250_create_comments.rb
+++ blog/db/migrate/20150701135250_create_comments.rb
@@ -3,9 +3,14 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string :commenter
       t.text :body
-      t.references :article, index: true, foreign_key: true
+
+      # this line adds an integer column called `article_id`.
+      t.references :article, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :comments, :articles
   end
 end
+
+
