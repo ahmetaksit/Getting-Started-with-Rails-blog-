fc3ac2f00651674195425923a87205ee8e57b4a1
Ahmet AKŞİT <ahmetaksit@gmail.com>
2015-07-02 11:13:56 +0300

    9.1 Basic Authentication

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "admin", password: "aksit", only: :destroy
+
   def create
       @article = Article.find(params[:article_id])
       @comment = @article.comments.create(comment_params)
